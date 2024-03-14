ae1466e18a232fb690c462e321f456d264194225
Nilesh <nileshk@whiteorangesoftware.com>
2024-03-14 15:24:16 +0530

    update license

--- lib/awesome_gradiant_font.dart
+++ lib/awesome_gradiant_font.dart
@@ -7,30 +7,30 @@ import 'package:flutter/material.dart';
 class AwesomeGradiantFont extends StatelessWidget {
   /// It is the root widget of your application.
   const AwesomeGradiantFont(
-  /// The [Text] widget to display.
-  /// this is text
-  /// is used to description
-  this.text, {
-  /// The colors the gradient should obtain at each of the stops.
-  ///
-  /// If [stops] is non-null, this list must have the same length as [stops].
-  ///
-  /// This list must have at least two colors in it (otherwise, it's not a
-  /// gradient!).
-  required this.gradient,
+    /// The [Text] widget to display.
+    /// this is text
+    /// is used to description
+    this.text, {
+    /// The colors the gradient should obtain at each of the stops.
+    ///
+    /// If [stops] is non-null, this list must have the same length as [stops].
+    ///
+    /// This list must have at least two colors in it (otherwise, it's not a
+    /// gradient!).
+    required this.gradient,
 
-  ///  It is used to make the font bold or italic in appearance.
-  ///  It determines the text's thickness.
-  ///  It determines the text's size.
-  ///  It is used to specify the font's typeface. We must first download a typeface file for our project and save it in the assets/font folder. Finally, set up the pubspec.yaml file so that it can be used in the project.
-  ///  It is used to make the font bold or italic in appearance
-  ///  It is used to determine the text's colour.
-  ///  It's used to figure out how far apart the text's characters are.
-  ///  It is used to indicate the distance between two text words.
-  ///  	It's used to create a background for the text.
-  ///  We use the three parameters decoration, decorationColor, and decorationStyle to decorate text. The decoration determines the location. ‘decorationColor’ specifies the colour and ‘decorationStyle’ specifies the shape.
-  ///  whole properties included in this package
-  this.style,
+    ///  It is used to make the font bold or italic in appearance.
+    ///  It determines the text's thickness.
+    ///  It determines the text's size.
+    ///  It is used to specify the font's typeface. We must first download a typeface file for our project and save it in the assets/font folder. Finally, set up the pubspec.yaml file so that it can be used in the project.
+    ///  It is used to make the font bold or italic in appearance
+    ///  It is used to determine the text's colour.
+    ///  It's used to figure out how far apart the text's characters are.
+    ///  It is used to indicate the distance between two text words.
+    ///  	It's used to create a background for the text.
+    ///  We use the three parameters decoration, decorationColor, and decorationStyle to decorate text. The decoration determines the location. ‘decorationColor’ specifies the colour and ‘decorationStyle’ specifies the shape.
+    ///  whole properties included in this package
+    this.style,
   });
 
   final String text;
